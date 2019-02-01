dd7ff511732fa654646f4a92ce2bc4f8db894ebd
makeupyourmind <nikitosnov@gmail.com>
2019-02-01 19:14:00 +0200

    ad

--- public/scripts/homePage/hide.js
+++ public/scripts/homePage/hide.js
@@ -1,4 +1,7 @@
-document.getElementById('newPassword').style.border = '1px solid grey';
-document.getElementById('newPassword').value = '';
+function hide() {
+
+  document.getElementById('newPassword').style.border = '1px solid grey';
+  document.getElementById('newPassword').value = '';
   document.getElementById('oldPassword').style.border = '1px solid grey';
   document.getElementById('oldPassword').value = '';
+}
--- public/scripts/homePage/hide2.js
+++ public/scripts/homePage/hide2.js
@@ -1,2 +1,5 @@
+function hide2()
+{
 document.getElementById('moreNewPassword').style.border = '1px solid grey';
 document.getElementById('moreNewPassword').value = '';
+}
